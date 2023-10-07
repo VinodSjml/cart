21d6bfa6e6018c651c2ba7a5b5b73406a94279f7
CodingManoj <mvp2612@hotmail.com>
2023-10-06 23:10:44 -0400

    Git Auto Push

--- mutable-infra/main.tf
+++ mutable-infra/main.tf
@@ -8,4 +8,5 @@ module "cart" {
   OD_INSTANCE_TYPE     = var.OD_INSTANCE_TYPE
   OD_INSTANCE_COUNT    = var.OD_INSTANCE_COUNT
   APP_PORT             = var.APP_PORT
+  INERNAL              = var.INERNAL 
 }
